28fce77b19d6f69e1f64773d4aac5ad7dc8b8ff8
Kevin Coxe <kevcoxe@gmail.com>
2020-03-25 10:33:41 -0400

    add redis

--- js/backend-node-express/index.js
+++ js/backend-node-express/index.js
@@ -1,18 +1,47 @@
 // server.js
-const fetch = require('node-fetch');
 const express = require('express');
-const app = express();
+const fetch = require('node-fetch');
+const redisClient = require('./redis-client');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Utils
+// =====================================
 
 const fetchData = async (url) => {
   return await fetch(url)
     .then(response => response.json())
     .catch(error => error);
-}
+};
+
+
+// Redis API
+// =====================================
+
+app.get('/api/store/:key', async (req, res) => {
+    const { key } = req.params;
+    const value = req.query;
+    await redisClient.setAsync(key, JSON.stringify(value));
+    return res.send('Success');
+});
+
+
+app.get('/api/:key', async (req, res) => {
+    const { key } = req.params;
+    const rawData = await redisClient.getAsync(key);
+    return res.json(JSON.parse(rawData));
+});
+
+
+// API
+// =====================================
 
 app.get('/api', (req, res) => {
   return res.send('hello, welcome to the api');
 });
+
 
 app.get('/api/posts', async (req, res) => {
   fetchData('https://jsonplaceholder.typicode.com/posts').then((data) => {
@@ -23,6 +52,7 @@ app.get('/api/posts', async (req, res) => {
   });
 });
 
+
 app.get('/api/comments', async (req, res) => {
   fetchData('https://jsonplaceholder.typicode.com/comments').then((data) => {
     return res.json(data);
@@ -31,6 +61,7 @@ app.get('/api/comments', async (req, res) => {
     return res.json(error);
   });
 });
+
 
 app.get('/api/photos', async (req, res) => {
   fetchData('https://jsonplaceholder.typicode.com/photos').then((data) => {
@@ -41,6 +72,7 @@ app.get('/api/photos', async (req, res) => {
   });
 });
 
+
 app.get('/api/todos', async (req, res) => {
   fetchData('https://jsonplaceholder.typicode.com/todos').then((data) => {
     return res.json(data);
@@ -49,6 +81,7 @@ app.get('/api/todos', async (req, res) => {
     return res.json(error);
   });
 });
+
 
 app.get('/api/users', async (req, res) => {
   fetchData('https://jsonplaceholder.typicode.com/users').then((data) => {
@@ -59,7 +92,9 @@ app.get('/api/users', async (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+
+// RUN
+// =====================================
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
