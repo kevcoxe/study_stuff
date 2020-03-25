0dfcd4d61643efbf68f785c1de68bd727f63c7ac
Kevin Coxe <kevcoxe@gmail.com>
2020-03-25 08:33:48 -0400

    connect frontend to backend api

--- js/backend-node-express/index.js
+++ js/backend-node-express/index.js
@@ -1,12 +1,65 @@
 // server.js
+const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 
+
+const fetchData = async (url) => {
+  return await fetch(url)
+    .then(response => response.json())
+    .catch(error => error);
+}
+
 app.get('/api', (req, res) => {
-    return res.send('Hello world');
+  return res.send('hello, welcome to the api');
+});
+
+app.get('/api/posts', async (req, res) => {
+  fetchData('https://jsonplaceholder.typicode.com/posts').then((data) => {
+    return res.json(data);
+  }).catch((error) => {
+    console.log(error)
+    return res.json(error);
+  });
+});
+
+app.get('/api/comments', async (req, res) => {
+  fetchData('https://jsonplaceholder.typicode.com/comments').then((data) => {
+    return res.json(data);
+  }).catch((error) => {
+    console.log(error)
+    return res.json(error);
+  });
+});
+
+app.get('/api/photos', async (req, res) => {
+  fetchData('https://jsonplaceholder.typicode.com/photos').then((data) => {
+    return res.json(data);
+  }).catch((error) => {
+    console.log(error)
+    return res.json(error);
+  });
+});
+
+app.get('/api/todos', async (req, res) => {
+  fetchData('https://jsonplaceholder.typicode.com/todos').then((data) => {
+    return res.json(data);
+  }).catch((error) => {
+    console.log(error)
+    return res.json(error);
+  });
+});
+
+app.get('/api/users', async (req, res) => {
+  fetchData('https://jsonplaceholder.typicode.com/users').then((data) => {
+    return res.json(data);
+  }).catch((error) => {
+    console.log(error)
+    return res.json(error);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- js/frontend-react/src/App.js
+++ js/frontend-react/src/App.js
@@ -1,14 +1,96 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
 
-  const [stuff, setStuff] = useState('');
+const getData = async (url, callback) => {
+  await axios.get(url)
+    .then(res => {
+      console.log(res.data);
+      callback(res.data);
+    })
+    .catch(err => console.log(err));
+};
 
-  const getStuff = (new_stuff) => {
-    setStuff(new_stuff);
+
+function Posts() {
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    await getData('/api/posts', setPosts);
   };
+
+  return (
+    <div>
+      <button onClick={() => getPosts()}>Get Posts</button>
+      {posts.map(post => {
+        return (<p key={post.id}>{ post.title }</p>)
+      })}
+    </div>
+  )
+}
+
+
+function Comments() {
+
+  const [comments, setComments] = useState([]);
+
+  const getComments = async () => {
+    await getData('/api/comments', setComments);
+  };
+
+  return (
+    <div>
+      <button onClick={() => getComments()}>Get Comments</button>
+      {comments.map(comment => {
+        return (<p key={comment.id}>{ comment.name }</p>)
+      })}
+    </div>
+  )
+}
+
+
+function Todos() {
+
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    await getData('/api/todos', setTodos);
+  };
+
+  return (
+    <div>
+      <button onClick={() => getTodos()}>Get Todos</button>
+      {todos.map(todo => {
+        return (<p key={todo.id}>{ todo.title }</p>)
+      })}
+    </div>
+  )
+}
+
+
+function Users() {
+
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    await getData('/api/users', setUsers);
+  };
+
+  return (
+    <div>
+      <button onClick={() => getUsers()}>Get Users</button>
+      {users.map(user => {
+        return (<p key={user.id}>{ user.name }</p>)
+      })}
+    </div>
+  )
+}
+
+
+function App() {
 
   return (
     <div className="App">
@@ -17,10 +99,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={() => getStuff('got stuff')}>Get stuff</button>
-        <p>
-          { stuff }
-        </p>
+        <Users />
+        <Posts />
+        <Comments />
+        <Todos />
       </header>
     </div>
   );
